8062ecb8fb93d9310da7f19b545a037ab6709843
tigote vitthal <vitthaltigote15@gmail.com>
2024-04-06 19:55:39 +0530

    files location edit

--- admincontrol/voterDelete.php
+++
@@ -1,7 +0,0 @@
-<?php  include('./includefiles/header.php'); ?>
-
-<h2 align='center'>Voter delete</h2>
-
-
-
-<?php  include('./includefiles/footer.php'); ?>
--- admincontrol/voterUpdate.php
+++
@@ -1,5 +0,0 @@
-<?php  include('header.php'); ?>
-
-<h2 align='center'Voter Update</h2>
-
-<?php  include('footer.php'); ?>
